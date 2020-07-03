0a5325743c57e09adb559bb08e7c1500f41acaa9
redline <ibrah1440@gmail.com>
2020-07-03 17:27:57 -0400

    init

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://ibraheem.ca',
+  pathPrefix: '/',
+  title: 'Blog by Ibraheem Ahmed',
+  subtitle: 'My personal website and blog where I write about software development',
+  copyright: '© All rights reserved.',
+  disqusShortname: 'ibraheem-3',
+  postsPerPage: 6,
+  googleAnalyticsId: 'UA-170786189-1',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Ibraheem Ahmed',
+    photo: '/photo.png',
+    bio: 'Hey, I\'m Ibraheem Ahmed. I’m a freelance developer from Toronto, Canada. I love working with Ruby on Rails and React.js.',
+    contacts: {
+      email: 'ibrah1440@gmail.com',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: '/redline-gh',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
