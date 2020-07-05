1305f94b6d26eab6a971e7a8762542ac4f1b1127
redline <ibrah1440@gmail.com>
2020-07-05 14:52:07 -0400

    buy me a coffee

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,15 +1,20 @@
 // @flow strict
-import React from 'react';
-import { getContactHref } from '../../../utils';
-import styles from './Author.module.scss';
-import { useSiteMetadata } from '../../../hooks';
+import React from "react";
+import { getContactHref } from "../../../utils";
+import styles from "./Author.module.scss";
+import { useSiteMetadata } from "../../../hooks";
+import BuyMeACoffee from "../../BuyMeACoffee";
 
 const Author = () => {
   const { author } = useSiteMetadata();
-  const bio = {__html: author.bio}
+  const bio = { __html: author.bio };
   return (
-    <div className={styles['author']}>
-      <p className={styles['author__bio']} dangerouslySetInnerHTML={{__html: author.bio}} />
+    <div className={styles["author"]}>
+      <p
+        className={styles["author__bio"]}
+        dangerouslySetInnerHTML={{ __html: author.bio }}
+      />
+      <BuyMeACoffee />
     </div>
   );
 };
