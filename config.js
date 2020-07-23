52ef6b5b04e2083e61c934acfce158eb33406e05
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-07-23 16:03:29 -0400

    add stackoverflow link

--- config.js
+++ config.js
@@ -1,49 +1,51 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://ibraheem.ca',
-  pathPrefix: '/',
-  title: 'Ibraheem Ahmed | Freelance software developer',
-  subtitle: 'A blog by Ibraheem Ahmed | Freelance software developer',
-  copyright: '© All rights reserved.',
-  disqusShortname: 'ibraheem-3',
+  url: "https://ibraheem.ca",
+  pathPrefix: "/",
+  title: "Ibraheem Ahmed | Freelance software developer",
+  subtitle: "A blog by Ibraheem Ahmed | Freelance software developer",
+  copyright: "© All rights reserved.",
+  disqusShortname: "ibraheem-3",
   postsPerPage: 6,
-  googleAnalyticsId: 'UA-170786189-1',
+  googleAnalyticsId: "UA-170786189-1",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'Contact me',
-      path: '/pages/contact'
+      label: "Contact me",
+      path: "/pages/contact",
     },
     {
-      label: 'Tags',
-      path: '/tags'
-    }
+      label: "Tags",
+      path: "/tags",
+    },
   ],
   author: {
-    name: 'Ibraheem Ahmed',
-    photo: '/photo.png',
-    bio: '<p>Hey, I\'m Ibraheem Ahmed. I’m a software freelance developer from Toronto, Canada. I love working with Ruby on Rails and React.js, and am currently working on <a href="https://github.com/ibraheemdev/agilely">agilely</a>, a open source project management app</p>',
+    name: "Ibraheem Ahmed",
+    photo: "/photo.png",
+    bio:
+      '<p>Hey, I\'m Ibraheem Ahmed. I’m a software freelance developer from Toronto, Canada. I love working with Ruby on Rails and React.js, and am currently working on <a href="https://github.com/ibraheemdev/agilely">agilely</a>, a open source project management app</p>',
     contacts: {
-      email: 'ibrah1440@gmail.com',
-      facebook: '',
-      telegram: '',
-      twitter: '',
-      github: 'ibraheemdev',
-      rss: '',
-      vkontakte: '',
-      linkedin: '',
-      instagram: '',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
+      email: "ibrah1440@gmail.com",
+      facebook: "",
+      telegram: "",
+      twitter: "",
+      github: "ibraheemdev",
+      rss: "",
+      vkontakte: "",
+      linkedin: "",
+      instagram: "",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+      stackoverflow: "8858995",
+    },
+  },
 };
