5a0742b42bdef3e2030ffa0f573cd8629250d968
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-07-25 18:42:54 -0400

    convert TagsListQuery from static to dynamic query to prevent caching

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,2 @@
 // @flow strict
 export { default as useSiteMetadata } from "./use-site-metadata";
-export { default as useTagsList } from "./use-tags-list";
--- src/templates/tags-list-template.js
+++ src/templates/tags-list-template.js
@@ -1,15 +1,15 @@
 // @flow strict
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 import kebabCase from "lodash/kebabCase";
 import Layout from "../components/Layout";
 import Sidebar from "../components/Sidebar";
 import Page from "../components/Page";
-import { useSiteMetadata, useTagsList } from "../hooks";
+import { useSiteMetadata } from "../hooks";
 
-const TagsListTemplate = () => {
+const TagsListTemplate = ({ data }) => {
   const { title, subtitle } = useSiteMetadata();
-  const tags = useTagsList();
+  const tags = data.allMarkdownRemark.group
 
   return (
     <Layout title={`Tags - ${title}`} description={subtitle}>
@@ -28,5 +28,18 @@ const TagsListTemplate = () => {
     </Layout>
   );
 };
+
+export const query = graphql`
+  query TagsListQuery {
+    allMarkdownRemark(
+      filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true } } }
+    ) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
 
 export default TagsListTemplate;
