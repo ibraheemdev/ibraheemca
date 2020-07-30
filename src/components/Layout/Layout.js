9fb5c120e966da629470f75db6ebce8b1c78f8a4
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-07-29 20:05:43 -0400

    update bmc widget

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -30,6 +30,17 @@ const Layout = ({ children, title, description, socialImage }: Props) => {
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={metaImageUrl} />
+        <script
+          data-name="BMC-Widget"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="ibraheem"
+          data-description="Any support is greatly appreciated."
+          data-message="Thank you for visiting. You can now buy me a coffee!"
+          data-color="#FF813F"
+          data-position="right"
+          data-x_margin="18"
+          data-y_margin="18"
+        />
       </Helmet>
       {children}
     </div>
--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,20 +1,16 @@
 // @flow strict
 import React from "react";
-import { getContactHref } from "../../../utils";
 import styles from "./Author.module.scss";
 import { useSiteMetadata } from "../../../hooks";
-import BuyMeACoffee from "../../BuyMeACoffee";
 
 const Author = () => {
   const { author } = useSiteMetadata();
-  const bio = { __html: author.bio };
   return (
     <div className={styles["author"]}>
       <div
         className={styles["author__bio"]}
         dangerouslySetInnerHTML={{ __html: author.bio }}
       />
-      <BuyMeACoffee />
     </div>
   );
 };
