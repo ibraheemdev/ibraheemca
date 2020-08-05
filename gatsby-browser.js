c115cb513f432fb8e0a5331722ec37d79d9dbf2e
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-08-05 17:40:00 -0400

    remove all .js files from git

--- gatsby-browser.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./src/assets/scss/init.scss");
-require("./static/css/prismjs/theme.min.css");
--- gatsby-node.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createPages = require("./gatsby/create-pages");
-exports.onCreateNode = require("./gatsby/on-create-node");
