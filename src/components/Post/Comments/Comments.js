45097681044cbbb5759f50d3715653096b020d12
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-08-05 09:04:55 -0400

    fix disqus embed render issue

--- src/components/Post/Comments/Comments.js
+++ src/components/Post/Comments/Comments.js
@@ -1,29 +1,25 @@
 // @flow strict
-import React from "react";
-import DiscussionEmbed from "disqus-react";
-import { useSiteMetadata } from "../../../hooks";
+import React from 'react';
+import { DiscussionEmbed } from 'disqus-react' 
+import { useSiteMetadata } from '../../../hooks';
 
 type Props = {
   postTitle: string,
-  postSlug: string,
+  postSlug: string
 };
 
 const Comments = ({ postTitle, postSlug }: Props) => {
   const { url, disqusShortname } = useSiteMetadata();
 
-  if (!disqusShortname) {
-    return null;
+  if (!disqusShortname) return null;
+
+  const disqusConfig = {
+    shortname: disqusShortname,
+    config: { identifier: postTitle, title: postTitle, url: url + postSlug},
   }
 
   return (
-    <ReactDisqusComments
-      shortname={disqusShortname}
-      config={{
-        identifier: postTitle,
-        title: postTitle,
-        url: url + postSlug,
-      }}
-    />
+    <DiscussionEmbed {...disqusConfig} /> 
   );
 };
 
