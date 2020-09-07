aec0e3f1f2a5e84f8ceb4d12cd1f026f0dac9888
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-09-07 13:35:14 -0400

    set postPerPage to 4

--- config.ts
+++ config.ts
@@ -6,7 +6,7 @@ module.exports = {
   copyright:
     '<p>This blog is open source. Check it out on <a href="https://github.com/ibraheemdev/ibraheemca">github!</a></p>',
   disqusShortname: "ibraheem-3",
-  postsPerPage: 5,
+  postsPerPage: 4,
   googleAnalyticsId: "UA-170786189-1",
   menu: [
     {
